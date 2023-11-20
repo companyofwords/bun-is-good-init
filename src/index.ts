647f3cba817cda5babe872e7c2e23e695fd076d0
Neil Puttick (N) <companyofwords@users.noreply.github.com>
2023-11-20 21:30:49 +0000

    not so basic

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { Elysia, t } from "elysia";
+import { plugin } from "./plugin";
 
 const PORT = process.env.PORT || 3000;
 
@@ -6,14 +7,40 @@ const app = new Elysia();
 
 const secretAppTitle: string = "Bun is good init";
 
+// Application
 app
   .decorate("secretAppName", secretAppTitle)
+  .decorate("getDate", () => Date.now())
+  .use(plugin)
+  .state({ id: 1, email: "j@jmail.com" })
   .get("/", async (handler) => {
     console.log(handler.request.url, "handler");
     return { message: "Hey Hey h" };
   })
   .get("/post/:id", async ({ params: { id } }) => {
     return { id: id, title: "Learn or Burn" };
+  })
+  .post("/post", ({ body, set, store }) => {
+    console.log(store);
+    set.status = 201;
+    return body;
+  })
+  .get("/track/*", () => {
+    return "Track Route";
+  })
+  .get("/tracks", ({ store, getDate }) => {
+    console.log(store, getDate());
+    console.log(store["plugin-version"]);
+    return new Response(
+      JSON.stringify({
+        tracks: ["Dancing Feet", "song 2 "],
+      }),
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
   })
   .listen(PORT);
 
---
+++ src/plugin.ts
@@ -0,0 +1,7 @@
+import { Elysia, t } from "elysia";
+
+// Define Plugin
+export const plugin = new Elysia()
+  .state("plugin-version", 1)
+  .get("/form-plugin", () => "Hi")
+  .get("/greet", () => "Hello Dave");
