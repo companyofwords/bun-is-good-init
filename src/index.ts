9bb4df0b18ec8e3d85e4efaec5aa6a80fbef02ee
Neil Puttick (N) <companyofwords@users.noreply.github.com>
2023-11-20 17:41:39 +0000

    initial

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,14 @@
-import { Elysia } from "elysia";
+import { Elysia, t } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const PORT = process.env.PORT || 3000;
+
+const app = new Elysia()
+  .decorate("appName", "Bun Is Good Init")
+  .get("/", async (handler) => {
+    console.log(handler.request.url, "handler");
+    return { message: "Hey Hey" };
+  })
+  .listen(PORT);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
