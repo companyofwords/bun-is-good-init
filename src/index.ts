fb504cd56f54e8cb8423a29e4323da2641f81c38
Neil Puttick (N) <companyofwords@users.noreply.github.com>
2023-11-20 20:06:07 +0000

    funky text

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,34 @@
 import { Elysia, Handler, t } from "elysia";
+import figlet from "figlet";
+
+const server = Bun.serve({
+  port: 3888,
+  fetch(request, server) {
+    const body = figlet.textSync("Amazing Sylvia Puttick");
+    return new Response(body);
+  },
+});
 
 const PORT = process.env.PORT || 3000;
 
 const app = new Elysia();
 
-const title = "Bun is good init";
+const secretAppTitle: string = "Bun is good init";
 
 app
-  .decorate("appName", title)
+  .decorate("secretAppName", secretAppTitle)
   .get("/", async (handler) => {
     console.log(handler.request.url, "handler");
     return { message: "Hey Hey h" };
   })
+  // group('/user', app =>
+  //   app.get('/get', async (handler) => {return { message: "Hey Hey h" }
+  // }),
+  //   app.get('/create', async (handler) => {return { message: "Hey Hey h" }
+  // }),
+  //   app.get('/update', async (handler) => {return { message: "Hey Hey h" }
+  // })
+  //)
   .listen(PORT);
 
 console.log(
