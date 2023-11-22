5271461558e16ff8ed8c09278fc2c0d4523914df
Neil Puttick (N) <companyofwords@users.noreply.github.com>
2023-11-22 13:27:52 +0000

    group --- test out

--- src/controllers/staff.ts
+++ src/controllers/staff.ts
@@ -8,50 +8,53 @@ import { StaffMembersDatabase } from "../db.js";
 export const staffController = new Elysia()
   .state("staff-version", 10001)
   .decorate("db", new StaffMembersDatabase())
-  .get("/staff", ({ db }) => db.getStaffMembers())
-  .post(
-    "/staff",
-    async ({ db, body }) => {
-      console.log(body);
-      const id = (await db.addStaffMember(body)).id;
-      console.log(id, "New Staff Member");
-      return { success: true, id };
-    },
-    {
-      body: staffMembers,
-      //response: staffMembers,
-    }
-  )
-  .put(
-    "/staff/:id",
-    ({ db, params, body }) => {
-      try {
-        db.updateStaffMember(parseInt(params.id), body);
-        return { success: true };
-      } catch (e) {
-        return { success: false };
-      }
-    },
-    {
-      body: staffMembers,
-      params: t.Object({
-        id: t.String(),
-      }),
-    }
-  )
-  .delete(
-    "/staff/:id",
-    ({ db, params }) => {
-      try {
-        db.deleteStaffMember(parseInt(params.id));
-        return { success: true };
-      } catch (e) {
-        return { success: false };
-      }
-    },
-    {
-      params: t.Object({
-        id: t.String(),
-      }),
-    }
+  .group("/staff", (app) =>
+    app
+      .get("/", ({ db }) => db.getStaffMembers())
+      .post(
+        "/",
+        async ({ db, body }) => {
+          console.log(body);
+          const id = (await db.addStaffMember(body)).id;
+          console.log(id, "New Staff Member");
+          return { success: true, id };
+        },
+        {
+          body: staffMembers,
+          //response: staffMembers,
+        }
+      )
+      .put(
+        "/:id",
+        ({ db, params, body }) => {
+          try {
+            db.updateStaffMember(parseInt(params.id), body);
+            return { success: true };
+          } catch (e) {
+            return { success: false };
+          }
+        },
+        {
+          body: staffMembers,
+          params: t.Object({
+            id: t.String(),
+          }),
+        }
+      )
+      .delete(
+        "/:id",
+        ({ db, params }) => {
+          try {
+            db.deleteStaffMember(parseInt(params.id));
+            return { success: true };
+          } catch (e) {
+            return { success: false };
+          }
+        },
+        {
+          params: t.Object({
+            id: t.String(),
+          }),
+        }
+      )
   );
